936b9dc0937aeb171cb610c1ed39d12ade3c9d05
federicochecozzi <tiamantex@hotmail.com>
2023-04-08 16:59:41 -0300

    Script de conversión 1

--- tidy_conversion1.py
+++ tidy_conversion1.py
@@ -8,3 +8,12 @@ Created on Sat Apr  8 15:11:33 2023
 import pandas as pd
 import os
 import glob
+
+wdir = r"C:\Users\tiama\OneDrive\Documentos\Maestría en minería y exploración de datos\Taller de Tesis 1\TT1"
+os.chdir(wdir)
+
+files = glob.glob('Datos/spc24Oct2019/*/*/*.csv')
+datalabels = {f : f.split("\\") for f in files}
+spectredf = pd.concat([pd.read_csv(file, sep = ';', header = 1, decimal = ',', usecols = [0, 1]).assign(Group = labels[1],Sample = labels[2] ,File = labels[3]) for file,labels in datalabels.items()], ignore_index=True).pipe(pd.pivot_table, values="Counts", index=["Group","Sample","File"],columns=["Wavelength [nm]"])
+os.makedirs('Datos procesados/spc24Oct2019', exist_ok=True)  
+spectredf.to_csv('Datos procesados/spc24Oct2019/Minería.csv', sep = ';', decimal = ',')
