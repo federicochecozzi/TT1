7a06133141d574fc1c0e3a179ca690af910f783f
federicochecozzi <tiamantex@hotmail.com>
2023-04-25 23:00:43 -0300

    Importancia de variables

--- feature_importance1.R
+++ feature_importance1.R
@@ -12,7 +12,56 @@ dtrain  <- lgb.Dataset( data= data.matrix(df %>% select(where(is.numeric))),
 
 model  <- lgb.train( data= dtrain, objective = "multiclass", num_class = 4)
 
-as.data.frame( lgb.importance(model) )
 tree_imp = lgb.importance(model)#, percentage = T)
+as.data.frame( tree_imp )
 lgb.plot.importance(tree_imp, measure = "Gain")
 
+WI <- as.numeric(substring(tree_imp$Feature,2))
+
+df_long1 <- df %>%
+  filter(File == "M14_01.csv") %>%
+  pivot_longer(cols = !c("Group","Sample","File"), names_to = "Wavelength", values_to = "Intensity") %>%
+  mutate(Wavelength = as.numeric(substring(Wavelength,2))) %>%
+  mutate(Important = Wavelength %in% WI[1:250])
+
+df_long1 %>%
+  ggplot(aes(x = Wavelength, y = Intensity)) + 
+  geom_line() + 
+  geom_point(data = df_long1 %>% filter(Important == TRUE), aes(x = Wavelength, y = Intensity, color = Important)) +
+  theme(legend.position="none")
+
+df_long2 <- df %>%
+  filter(File == "M6_01.csv") %>%
+  pivot_longer(cols = !c("Group","Sample","File"), names_to = "Wavelength", values_to = "Intensity") %>%
+  mutate(Wavelength = as.numeric(substring(Wavelength,2))) %>%
+  mutate(Important = Wavelength %in% WI[1:250])
+
+df_long2 %>%
+  ggplot(aes(x = Wavelength, y = Intensity)) + 
+  geom_line() + 
+  geom_point(data = df_long2 %>% filter(Important == TRUE), aes(x = Wavelength, y = Intensity, color = Important)) +
+  theme(legend.position="none")
+
+df_long3 <- df %>%
+  filter(File == "M13_01.csv") %>%
+  pivot_longer(cols = !c("Group","Sample","File"), names_to = "Wavelength", values_to = "Intensity") %>%
+  mutate(Wavelength = as.numeric(substring(Wavelength,2))) %>%
+  mutate(Important = Wavelength %in% WI[1:250])
+
+df_long3 %>%
+  ggplot(aes(x = Wavelength, y = Intensity)) + 
+  geom_line() + 
+  geom_point(data = df_long3 %>% filter(Important == TRUE), aes(x = Wavelength, y = Intensity, color = Important)) +
+  theme(legend.position="none")
+
+df_long4 <- df %>%
+  filter(File == "M4_01.csv") %>%
+  pivot_longer(cols = !c("Group","Sample","File"), names_to = "Wavelength", values_to = "Intensity") %>%
+  mutate(Wavelength = as.numeric(substring(Wavelength,2))) %>%
+  mutate(Important = Wavelength %in% WI[1:250])
+
+df_long4 %>%
+  ggplot(aes(x = Wavelength, y = Intensity)) + 
+  geom_line() + 
+  geom_point(data = df_long4 %>% filter(Important == TRUE), aes(x = Wavelength, y = Intensity, color = Important)) +
+  theme(legend.position="none")
